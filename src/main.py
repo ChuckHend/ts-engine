312e9fd57a4fc7e88a08c6a6472b0a6758017951
ChuckHend <hendel.adam@gmail.com>
2017-09-24 21:07:46 -0500

    observation scaling

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from math import sqrt
 from numpy import concatenate
 from sklearn.metrics import mean_squared_error
-
+import pandas as pd
 import os
 os.chdir('c:/Users/hende/onedrive/analytics/finance/4cast/src')
 import featureEng as fe
@@ -20,10 +20,11 @@ n_out = 1
 # load dataset
 # dataset = getStocks.get_single(ticker=ticker, save=True)
 dataset = getStocks.load_single(ticker)
-
+dataset.rename(columns={'Adj Close':'AdjCls'}, inplace=True)
 
 ## Generate new features
 dataset = fe.derivative(dataset, fill_na = True)
+
 features = dataset.columns
 
 # Plot the features
@@ -39,20 +40,47 @@ towards scaling to the sequence length (ie past 10 days) and include features th
 will have information related to the overall period'''
 '''could also convert simply to stationary'''
 
-
-scaler = MinMaxScaler(feature_range=(-1, 1))
-
-'''scale according to the window --- n_in to n_out'''
-for row in range(dataset.shape[0]):
-    
-
+''' should select a range to look back for scaling and select a different 
+    range for the n_in and n_out...something like -30 or 1 year for scaling, then
+    select n_in and n_out from the scaled dataset'''
+''' steps:
+    # d = series_to_supervised for scaled_in and scaled_out
+      d = filter d for n_in and n_out (n_in < scaled_in, n_out < scaled_out)'''
 # scaled = scaler.fit_transform(dataset)
 
 # frame as supervised learning
-reframed = ps.series_to_supervised(scaled, n_in=n_in, n_out=n_out, 
+reframed = ps.series_to_supervised(dataset, n_in=n_in, n_out=n_out, 
                                    features=features)
 
 
+#TODO: package to function
+# note, can also keep a dictionary that maintains unscaling information
+# iterate over every row
+scaler = MinMaxScaler(feature_range=(-1, 1))
+scaled=pd.DataFrame(columns=reframed.columns)
+for row in range(reframed.shape[0]):
+    d=reframed.iloc[row,:]
+    d=pd.DataFrame(d.values.reshape(1,d.shape[0]), columns=reframed.columns)
+    # iterate over each feature
+    # scaling each feature against over all time in observation
+    featdf=pd.DataFrame()
+    print('Processing row: {}....{} complete'.format(row, row/reframed.shape[0]))
+    for feat in features:
+        #select all the features with partial match
+        vec=d.columns.to_series().str.contains(feat)
+        vec=d.columns[vec]
+        d1=d[vec]
+        # reshape for minmaxscaler (needs to shape off columns)
+        # then scale the feature
+        shaped=d1.values.reshape(d1.shape[-1],1)
+        scal = scaler.fit_transform(shaped)
+        scal = scal.reshape(1,scal.shape[0])
+        scal = pd.DataFrame(scal, columns = vec)
+        featdf=pd.concat([featdf, scal], axis=1)
+        
+    #print(featdf)
+    scaled=scaled.append(featdf, ignore_index=True)
+###END TODO:
 
 # create the list of features we dont want to predict
 # so take all features, less the one we want to predict
