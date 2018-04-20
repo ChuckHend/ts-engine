908cbe40ae0a823c042edab195ea261de85b67cd
ChuckHend <hendel.adam@gmail.com>
2018-04-19 21:45:33 -0500

    dev piece for read logs

---
+++ src/consolidate_files.py
@@ -0,0 +1,18 @@
+import glob
+
+
+fdir = '../data/30_second/'
+
+files = glob.glob(fdir + '*')
+
+def strip_ticker(s):
+    return s.strip(fdir).split('_')[0]
+
+tickers = list(set([strip_ticker(x) for x in files]))
+
+tickers = {key:[] for key in tickers}
+
+for file in files:
+    tickers[strip_ticker(file)].append(file)
+
+
