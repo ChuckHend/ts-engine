d3c137a6d9b57193ad44cea56894644dcf36b4b7
Ian Stanton <stant099@umn.edu>
2018-05-17 14:14:01 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='forecast-engine',
+      version='0.1dev',
+      long_description=open('README.md').read(),
+      author='Adam Hendel',
+      author_email='hendel.adam@gmail.com',
+      url='https://github.com/ChuckHend/forecast-engine',
+      packages=['distutils', 'distutils.command'],
+      license='GNU General Public License v3.0'
+     )
