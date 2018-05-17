d83af9d333a6149418a3e009c446b6cface60d6d
Ian Stanton <stant099@umn.edu>
2018-05-17 14:57:49 -0500

    Edit setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,19 @@ setup(name='forecast-engine',
       author_email='hendel.adam@gmail.com',
       url='https://github.com/ChuckHend/forecast-engine',
       packages=['src'],
+      install_requires=[
+      	'os', 
+      	'time', 
+      	'datetime', 
+      	'numpy', 
+      	'pandas', 
+      	'pandas-datareader', 
+      	'matplotlib', 
+      	'scikit-learn', 
+      	'scipy', 
+      	'bs4', 
+      	'keras', 
+      	'tensorflow-gpu'
+      ],
       license='GNU General Public License v3.0'
      )
